dbbb9d3829c5be499f5da7dcb57438672136c366
Andrew <andrewsolitei@gmail.com>
2023-12-05 10:38:19 +0300

    Create 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Ensure SSH client configuration uses the private key ~/.ssh/school
+file_line { 'Declare identity file':
+  path   => '/user/abbot/.ssh/config',
+  line   => 'IdentityFile .ssh/school',
+  ensure => present,
+}
+
+# Ensure SSH client configuration refuses to authenticate using a password
+file_line { 'Turn off passwd auth':
+  path   => '/user/abbot/.ssh/config',
+  line   => 'PasswordAuthentication no',
+  ensure => present,
+}
