d32cac9ed78f77465cd45759d550b7f0aee869e6
Andrew <andrewsolitei@gmail.com>
2023-12-09 00:51:23 +0300

    Update 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,11 @@
-# Ensure SSH client configuration uses the private key ~/.ssh/school
-file_line { 'Declare identity file':
-  path   => '/user/abbot/.ssh/config',
-  line   => 'IdentityFile school',
-  ensure => present,
+# 100-puppet_ssh_config.pp
+
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
 }
 
-# Ensure SSH client configuration refuses to authenticate using a password
-file_line { 'Turn off passwd auth':
-  path   => '/user/abbot/.ssh/config',
-  line   => 'PasswordAuthentication no',
-  ensure => present,
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile .ssh/school',
 }
