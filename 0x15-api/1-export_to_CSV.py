19686b5c168a79949bc072b8cba90644bce486fc
Andrew <andrewsolitei@gmail.com>
2024-01-03 16:29:08 +0300

    Update 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,44 +1,33 @@
-import sys
-import requests
+#!/usr/bin/python3
+"""Gather data about completed tasks for specific user,
+ and export data to CSV format"""
+
 import csv
+import requests
+import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2 or not sys.argv[1].isdigit():
-        print("Usage: python script.py <user_id>")
-        sys.exit(1)
 
-    user_id = sys.argv[1]
-    employee = requests.get('https://jsonplaceholder.typicode.com/users', params={'id': user_id})
-    tasks = requests.get('https://jsonplaceholder.typicode.com/todos', params={'userId': user_id})
+    if sys.argv[1].isdigit():
 
-    if employee.status_code != 200 or tasks.status_code != 200:
-        print("Error fetching data. Please check the user ID.")
-        sys.exit(1)
+        user_id = sys.argv[1]
+        employee = requests.get('https://jsonplaceholder.typicode.com/users',
+                                params={'id':  user_id})
+        tasks = requests.get('https://jsonplaceholder.typicode.com/todos',
+                             params={'userId':  user_id})
 
-    user_name = employee.json()[0].get('name')
-    total_tasks = len(tasks.json())
-    done_tasks = 0
+        user_name = employee.json()[0].get('username')
 
-    # Count done tasks
-    completed_tasks = []
-
-    for task in tasks.json():
-        if task.get('completed'):
-            done_tasks += 1
-            completed_tasks.append({
-                "USER_ID": user_id,
-                "USERNAME": user_name,
-                "TASK_COMPLETED_STATUS": "Completed",
-                "TASK_TITLE": task.get('title')
-            })
-
-    # Export data to CSV
-    csv_file_name = "{}.csv".format(user_id)
-    with open(csv_file_name, mode='w', newline='') as csv_file:
-        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-        writer.writeheader()
-        writer.writerows(completed_tasks)
-
-    print("Employee {} is done with tasks({}/{}). CSV file '{}' created.".format(user_name, done_tasks, total_tasks, csv_file_name))
+        with open('{}.csv'.format(user_id), 'w', newline='') as o:
+            f = ["USER_ID", "USERNAME\
+", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+            writer = csv.DictWriter(o, fieldnames=f, quoting=csv.QUOTE_ALL)
+            for task in tasks.json():
+                result = {"USER_ID": user_id, "USERNAME\
+": user_name, "TASK_TITLE": task.get('title')}
+                if task.get('completed'):
+                    result["TASK_COMPLETED_STATUS"] = True
+                    writer.writerow(result)
+                else:
+                    result["TASK_COMPLETED_STATUS"] = False
+                    writer.writerow(result)
